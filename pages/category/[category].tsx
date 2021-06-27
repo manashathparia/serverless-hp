181c9018ba49537147e65e43e9d9c28ec073cd5b
Manash athparia <athmanash@gmail.com>
2021-06-28 00:12:44 +0530

    commented unsued code

--- pages/category/[category].tsx
+++ pages/category/[category].tsx
@@ -1,14 +1,22 @@
-import { getPostsByCategory } from "../../apiHandlers/posts";
-import { wrapper } from "../../redux/Reducers";
-import { getAllPostsAction } from "../../redux/Actions/Posts.actions";
+// import { getPostsByCategory } from "../../apiHandlers/posts";
+// import { wrapper } from "../../redux/Reducers";
+// import { getAllPostsAction } from "../../redux/Actions/Posts.actions";
 
 export default function CategoryPage() {
 	return <div></div>;
 }
-export const getStaticProps = wrapper.getStaticProps(
-	async ({ store, params }: any) => {
-		let posts: any = await getPostsByCategory(params.id);
-		posts = JSON.parse(JSON.stringify(posts[0])); //https://github.com/vercel/next.js/issues/11993#issuecomment-617375501
-		store.dispatch(getAllPostsAction(posts));
-	}
-);
+// export const getStaticProps = wrapper.getStaticProps(
+// 	async ({ store, params }: any) => {
+// 		let posts: any = await getPostsByCategory(params.id);
+// 		posts = JSON.parse(JSON.stringify(posts[0])); //https://github.com/vercel/next.js/issues/11993#issuecomment-617375501
+// 		store.dispatch(getAllPostsAction(posts));
+// 	}
+// );
+
+// export const getStaticPaths = async () => {
+// 	const path = {params: {category: ""}}
+// 	return {
+// 		[path],
+// 		fallback: false,
+// 	};
+// };
